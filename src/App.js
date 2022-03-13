d445d8a7f27ec95257851070701c228189554041
usersantiago <usersantiago27@gmail.com>
2022-03-12 21:47:51 -0500

    Estamos editando la interfaz de usuario para que nos permita cambiar el nombre a la tarea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import {nanoid} from "nanoid";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  export default function App(props) {
+    const [tasks, setTasks] = useState(props.tasks);
+
+    function addTask(name){
+      const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+      setTasks([...tasks, newTask]);
+    }
+
+    const taskList = tasks.map(task => (
+      <Todo
+        id={task.id} name={task.name}
+        completed={task.completed} key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+    const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
+    
+    function toggleTaskCompleted(id){
+      const updatedTasks = tasks.map(task => {
+        if(id === task.id){
+          return {...task, completed: !task.completed}
+        }
+        return task;
+      });
+      setTasks(updatedTasks);
+    }
+
+    function deleteTask(id){
+      const remainingTasks = tasks.filter(task => id !== task.id);
+      setTasks(remainingTasks);
+    }
+
+    function editTask(id, newName){
+      const editedTaskList = tasks.map(task => {
+        if(id === task.id){
+          return {...task, name: newName}
+        }
+        return task;
+      });
+      setTasks(editedTaskList);
+    }
+
+    return (
+      <div className="todoapp stack-large">
+        <h1>TodoMatic</h1>
+
+        <Form addTask={addTask}/>
+
+        <div className="filters btn-group stack-exception">
+
+          <FilterButton/>
+          <FilterButton/>
+          <FilterButton/>
+
+        </div>
+        <h2 id="list-heading">{headingText}</h2>
+        <ul role="list" className="todo-list stack-large stack-exception"
+          aria-labelledby="list-heading"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          {taskList}
+        </ul>
+      </div>
+    );
+  }  
 
-export default App;
